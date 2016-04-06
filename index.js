950d3a10f0612d05b61f360aaa22a84fd950c8c0
Ramon Bezerra <ramon.bezerra@booking.com>
2016-04-06 21:58:51 +0200

    chaining and different types of arguments working

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var arr = [];
 
-function rQuery(s) {
-	var elements = getSelector(s);
+function rQuery(option) {
+	var elements = getElements(option);
 
+	if (typeof elements === 'undefied') {
+		throw new Error('[rQuery] invalid argument: ' + option);
+	}
+
+	// Mimic array
 	for (var i = 0; i < elements.length; i++) {
 		this[i] = elements[i];
 	}
@@ -10,27 +15,19 @@ function rQuery(s) {
 	this.length = elements.length;
 }
 
-// Private
-// ----------------
-function getSelector(s) {
-	return [].slice.call(document.querySelectorAll(s));
-}
-
-function extend(a, b) {
-	for (var key in b) {
-		if (b.hasOwnProperty(key)) {
-			a[key] = b[key];
-		}
-	}
-
-	return a;
-}
-
 // Public
 // ----------------
 var api = {
 	get: function(index) {
+		if (typeof index === 'undefined') {
+			return [].slice.call(this);
+		}
+
 		return this[index];
+	},
+
+	eq: function(index) {
+		return new rQuery(this[index]);
 	},
 
 	html: function() {
@@ -52,6 +49,54 @@ api.indexOf = arr.indexOf;
 
 // Extending rQuery
 extend(rQuery.prototype, api);
+
+// Private
+// ----------------
+function getSelector(s) {
+	return [].slice.call(document.querySelectorAll(s));
+}
+
+function getElements(arg) {
+	var list;
+
+	if (typeof arg === 'object') {
+		if (arg.nodeType === 1) {
+			list = [arg];
+		} else if (arg instanceof rQuery){
+			list =  arg;
+		} else if (isNodeList(arg)) {
+			list = [].slice.call(arg);
+		}
+	} else if(typeof arg === 'string') {
+		list = getSelector(arg);
+	}
+
+	return list;
+}
+
+function isNodeList(object) {
+	var result = Object.prototype.toString.call(object);
+
+	// '[object Array]' condition is added here
+	// to accept the way jsdom understands NodeList.
+	if (result === '[object HTMLCollection]' ||
+		result === '[object NodeList]' ||
+		result === '[object Array]') {
+		return true;
+	}
+
+	return false;
+}
+
+function extend(a, b) {
+	for (var key in b) {
+		if (b.hasOwnProperty(key)) {
+			a[key] = b[key];
+		}
+	}
+
+	return a;
+}
 
 // Exports
 // ----------------
